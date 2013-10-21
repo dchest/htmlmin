eb7ed59e29471b44dadefd88d06614795b728b9b
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-10-21 18:17:19 +0200

    Initial commit.

---
+++ htmlmin.go
@@ -0,0 +1,118 @@
+// Copyright 2013 Dmitry Chestnykh. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package htmlmin minifies HTML.
+package htmlmin
+
+import (
+	"bytes"
+	"io"
+
+	"code.google.com/p/go.net/html"
+)
+
+// Minify returns minified version of the given HTML data.
+func Minify(data []byte) (out []byte, err error) {
+	var b bytes.Buffer
+	z := html.NewTokenizer(bytes.NewReader(data))
+	raw := false
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			err := z.Err()
+			if err == io.EOF {
+				return b.Bytes(), nil
+			}
+			return nil, err
+		case html.StartTagToken, html.SelfClosingTagToken:
+			tagName, hasAttr := z.TagName()
+			raw = isRawTagName(tagName)
+			b.WriteByte('<')
+			b.Write(tagName)
+			var k, v []byte
+			isFirst := true
+			for hasAttr {
+				k, v, hasAttr = z.TagAttr()
+				if isFirst {
+					b.WriteByte(' ')
+					isFirst = false
+				}
+				b.Write(k)
+				if len(v) == 0 {
+					// Empty attribute values can be skipped.
+					continue
+				}
+				b.WriteByte('=')
+				if quoteChar := valueQuoteChar(v); quoteChar != 0 {
+					// Quoted value.
+					b.WriteByte(quoteChar)
+					b.Write(v)
+					b.WriteByte(quoteChar)
+				} else {
+					// Unquoted value.
+					b.Write(v)
+				}
+			}
+			b.WriteByte('>')
+		case html.EndTagToken:
+			tagName, _ := z.TagName()
+			raw = false
+			b.Write([]byte("</"))
+			b.Write(tagName)
+			b.WriteByte('>')
+		case html.CommentToken:
+			// skip
+		case html.TextToken:
+			if raw {
+				b.Write(z.Raw())
+			} else {
+				b.Write(trimTextToken(z.Raw()))
+			}
+		default:
+			b.Write(z.Raw())
+		}
+
+	}
+}
+
+func isRawTagName(tagName []byte) bool {
+	switch string(tagName) {
+	case "script", "pre", "code":
+		return true
+	default:
+		return false
+	}
+}
+
+func trimTextToken(b []byte) (out []byte) {
+	out = make([]byte, 0)
+	seenSpace := true
+	for _, c := range b {
+		switch c {
+		case ' ', '\n', '\r', '\t':
+			if !seenSpace {
+				out = append(out, c)
+				seenSpace = true
+			}
+		default:
+			out = append(out, c)
+			seenSpace = false
+		}
+	}
+	if len(out) > 1 && seenSpace {
+		out = out[:len(out)-1]
+	}
+	return out
+}
+
+func valueQuoteChar(b []byte) byte {
+	if bytes.IndexAny(b, "'`=<> \n\r\t\b") != -1 {
+		return '"' // quote with quote mark
+	}
+	if bytes.IndexByte(b, '"') != -1 {
+		return '\'' // quote with apostrophe
+	}
+	return 0 // do not quote
+}
---
+++ htmlmin_test.go
@@ -0,0 +1,46 @@
+package htmlmin
+
+import (
+	"fmt"
+	"testing"
+)
+
+var doc = `<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <title>Sample document</title>
+</head>
+<body>
+   <!-- This is a comment -->
+   <p  class="quoted value">
+     Hello, this is a document.<br/>About
+     something.
+   </p>
+   <footer>
+      Copyright &copy;    Decent    People
+   </footer>
+   <script>
+     (function()
+       alert("Please   leave   this   unchanged! Thanks");
+     )();
+   </script>
+</body>
+</html>`
+
+var miniDoc = `<!doctype html><head><meta charset=utf-8><title>Sample document</title></head><body><p class="quoted value">Hello, this is a document.<br>About
+something.</p><footer>Copyright &copy; Decent People</footer><script>
+     (function()
+       alert("Please   leave   this   unchanged! Thanks");
+     )();
+   </script></body></html>`
+
+func TestMinify(t *testing.T) {
+	result, err := Minify([]byte(doc))
+	if err != nil {
+		t.Fatalf("error: %s", err)
+	}
+	if string(result) != miniDoc {
+		t.Errorf("incorrect result of minifying")
+	}
+	fmt.Printf("---\n%s\n---\n", result)
+}
